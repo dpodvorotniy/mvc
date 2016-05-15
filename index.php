ea258da1c3ff15fdff78a7f717116a05cf57bd47
dpodvorotniy <dpodvorotniy@gmail.com>
2016-05-15 20:18:21 +0300

    addGitignore

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DS',DIRECTORY_SEPARATOR);
+define('ROOT',__DIR__.DS);
+define('VIEW_DIR', ROOT.'view'.DS);
+define('LIB_DIR', ROOT.'library'.DS);
+define('CONTROL_DIR', ROOT.'controller'.DS);
+define('MODEL_DIR', ROOT.'model'.DS);
+//---my
